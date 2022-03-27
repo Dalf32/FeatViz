a9edc93dba1ce9a1e43a3cf5664a3b506484d586
Dalf32 <kylepmullins@gmail.com>
2022-03-27 02:29:14 -0400

    Added code to clean up some rare cases where a class feature prerequisite was being mapped to a feat that shared a word

--- lib/featviz/entities/feat_prereq.rb
+++ lib/featviz/entities/feat_prereq.rb
@@ -2,7 +2,14 @@ class FeatPrereq < Hanami::Entity
   def self.feat_name_from_prereq_text(prereq_text)
     regex = /(?<feat_name>[A-Z][’'\w-]+((\s?(of|and|the|nor|in|a|on|or|with the))*\s[A-Z][’'\w-]+)*)/
     matches = regex.match(prereq_text)
-    matches.nil? ? prereq_text : matches['feat_name'].strip.gsub('’', "'")
+    return prereq_text if matches.nil?
+
+    dc_text = prereq_text.downcase
+    if dc_text.end_with?('class feature') && !dc_text.include?(' or ')
+      return prereq_text
+    end
+
+    matches['feat_name'].strip.gsub('’', "'")
   end
 
   def to_hash
